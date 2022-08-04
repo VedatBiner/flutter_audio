f69f7c7690ee0b9f8bb4a0a385b28bf55e3abd09
Vedat Biner <vbiner@gmail.com>
2022-08-04 15:04:04 +0300

    Ses denemeleri

---
+++ README.md
@@ -0,0 +1,7 @@
+# flutter_audio
+
+[https://pub.dev/packages/audioplayers]()
+adresindeki örnek uygulama
+Fiziksel cihaz gerekebilir. Sanal cihazda ses çıkmadı.\
+![ScreenShot](/screen_shots/img.png)\
+
---
+++ lib/utils.dart
@@ -0,0 +1,32 @@
+// import 'package:audioplayers_example/components/dlg.dart';
+import 'package:flutter/material.dart';
+
+import 'components/dlg.dart';
+
+extension StateExt<T extends StatefulWidget> on State<T> {
+  void toast(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
+  void simpleDialog(String message, [String action = 'Ok']) {
+    showDialog<void>(
+      context: context,
+      builder: (_) {
+        return SimpleDlg(message: message, action: action);
+      },
+    );
+  }
+
+  void dialog(List<Widget> children) {
+    showDialog<void>(
+      context: context,
+      builder: (_) {
+        return Dlg(children: children);
+      },
+    );
+  }
+}
